1de2ca21c6de88d08716a6f159b906734dff81ce
Abishake <33455238+Abishake123@users.noreply.github.com>
2026-07-29 11:04:26 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let left = 0;
+        let right = heights.length - 1;
+        let maxWater = 0;
+
+        while (left < right) {
+            let width = right - left;
+            let height = Math.min(heights[left], heights[right]);
+            let currentWater = width * height;
+            maxWater = Math.max(maxWater, currentWater);
+
+            if (heights[left] < heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+}
